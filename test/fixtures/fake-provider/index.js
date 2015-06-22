af89d97d6662653060e69cf23252cd22d2f7d5b1
RaulJimenezOrtega <hhkaos@gmail.com>
2015-06-22 23:50:38 +0200

    Eliminando Koop y añadiendo datos de las estaciones de metro

--- test/fixtures/fake-provider/index.js
+++
@@ -1,5 +0,0 @@
-exports.name = 'test-provider';
-exports.pattern = '/:id';
-exports.controller = require('./controller');
-exports.routes = require('./routes');
-exports.model = require('./models/fake.js');
